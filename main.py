c865173353d7b352c0b3f6b60b987cac171cc3d4
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-09-18 13:39:35 +0300

    use make_pipeline

--- main.py
+++ main.py
@@ -110,63 +110,72 @@ vertex_buffer = ctx.buffer(assets['SpaceShip'])
 
 uniform_buffer = ctx.buffer(size=64)
 
-pipeline = ctx.pipeline(
-    vertex_shader='''
-        #version 330 core
 
-        layout (std140) uniform Common {
-            mat4 mvp;
-        };
+def make_pipeline(vertex_index, vertex_count):
+    return ctx.pipeline(
+        vertex_shader='''
+            #version 330 core
 
-        layout (location = 0) in vec3 in_vertex;
-        layout (location = 1) in vec3 in_normal;
-        layout (location = 2) in vec3 in_color;
+            layout (std140) uniform Common {
+                mat4 mvp;
+            };
 
-        out vec3 v_vertex;
-        out vec3 v_normal;
-        out vec3 v_color;
+            layout (location = 0) in vec3 in_vertex;
+            layout (location = 1) in vec3 in_normal;
+            layout (location = 2) in vec3 in_color;
 
-        void main() {
-            v_vertex = in_vertex;
-            v_normal = in_normal;
-            v_color = in_color;
-            gl_Position = mvp * vec4(v_vertex, 1.0);
-        }
-    ''',
-    fragment_shader='''
-        #version 330 core
+            out vec3 v_vertex;
+            out vec3 v_normal;
+            out vec3 v_color;
 
-        in vec3 v_vertex;
-        in vec3 v_normal;
-        in vec3 v_color;
+            void main() {
+                v_vertex = in_vertex;
+                v_normal = in_normal;
+                v_color = in_color;
+                gl_Position = mvp * vec4(v_vertex, 1.0);
+            }
+        ''',
+        fragment_shader='''
+            #version 330 core
 
-        layout (location = 0) out vec4 out_color;
+            in vec3 v_vertex;
+            in vec3 v_normal;
+            in vec3 v_color;
 
-        void main() {
-            vec3 light = vec3(4.0, 3.0, 10.0);
-            float lum = dot(normalize(light), normalize(v_normal)) * 0.7 + 0.3;
-            out_color = vec4(pow(v_color * lum, vec3(1.0 / 2.2)), 1.0);
-        }
-    ''',
-    layout=[
-        {
-            'name': 'Common',
-            'binding': 0,
-        },
-    ],
-    resources=[
-        {
-            'type': 'uniform_buffer',
-            'binding': 0,
-            'buffer': uniform_buffer,
-        },
-    ],
-    framebuffer=[image, depth],
-    topology='triangles',
-    cull_face='back',
-    vertex_buffers=zengl.bind(vertex_buffer, '3f 3f 3f', 0, 1, 2),
-    vertex_count=vertex_buffer.size // zengl.calcsize('3f 3f 3f'),
-)
+            layout (location = 0) out vec4 out_color;
+
+            void main() {
+                vec3 light = vec3(4.0, 3.0, 10.0);
+                float lum = dot(normalize(light), normalize(v_normal)) * 0.7 + 0.3;
+                out_color = vec4(pow(v_color * lum, vec3(1.0 / 2.2)), 1.0);
+            }
+        ''',
+        layout=[
+            {
+                'name': 'Common',
+                'binding': 0,
+            },
+        ],
+        resources=[
+            {
+                'type': 'uniform_buffer',
+                'binding': 0,
+                'buffer': uniform_buffer,
+            },
+        ],
+        framebuffer=[image, depth],
+        topology='triangles',
+        cull_face='back',
+        vertex_buffers=zengl.bind(vertex_buffer, '3f 3f 3f', 0, 1, 2),
+        first_vertex=vertex_index,
+        vertex_count=vertex_count,
+    )
+
+
+pipelines = {
+    'SpaceShip': make_pipeline(0, 840),
+    'Canister': make_pipeline(0, 0),
+}
 
 
 def render():
@@ -176,7 +185,8 @@ def render():
     depth.clear()
     camera = zengl.camera((3.0, 2.0, 2.0), (0.0, 0.0, 0.0), aspect=window.aspect, fov=45.0)
     uniform_buffer.write(camera)
-    pipeline.render()
+    pipelines['SpaceShip'].render()
+    pipelines['Canister'].render()
     image.blit()
     ctx.end_frame()
 
